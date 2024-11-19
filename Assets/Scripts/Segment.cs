bffc058c4f6407a68817282e7360ca24d2c6b247
bhunt02 <bridgette.hunt02@outlook.com>
2024-11-19 10:35:05 -0800

    added zone bounds & calculation of centroids (for later), bounding boxes that show when the corresponding pie segment is hovered over, reworked how segment text is rendered (used to be always 8 and in each segment, now it's 1-per-segment which renders at the centre of it)

--- Assets/Scripts/Segment.cs
+++ Assets/Scripts/Segment.cs
@@ -1,39 +1,72 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class Segment : MonoBehaviour
 {
+    [SerializeField] private GameObject segmentText;
+    private GameObject currentText;
     public LineRenderer segmentRenderer;
-    
     public PolygonCollider2D segmentCollider;
     private int SegmentNumber;
-    
-    
-    
+    private TargetManager targetManager;
+    private Vector2 goalPosition;
+    private Vector4 zoneBounds;
+    private GameObject[] boundingBoxes = new GameObject[4];
+    private Gradient boundingBoxColor = new Gradient();
     Gradient onHover = new Gradient();
     // Start is called before the first frame update
     void Start()
     {
-        
         float alpha = 1.0f;
         onHover.SetKeys(
             new GradientColorKey[] { new GradientColorKey(Color.yellow, 0.0f), new GradientColorKey(Color.yellow, 1.0f) },
             new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
-            );
-        
-        
+        );
+        boundingBoxColor.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(Color.magenta, 0.0f), new GradientColorKey(Color.magenta, 0.0f)},
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f)}
+        );
+        for (int i = 0; i < 4; i++)
+        {
+            boundingBoxes[i] = new GameObject();
+            boundingBoxes[i].name = "BoundingBox" + i;
+            var lr = boundingBoxes[i].AddComponent<LineRenderer>();
+            lr.colorGradient = boundingBoxColor;
+            boundingBoxes[i].transform.parent = transform;
+            boundingBoxes[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!targetManager)
+        {
+            var g = GameObject.Find("TargetManager");
+            targetManager = g.GetComponent<TargetManager>();   
+        }
+
+        UpdateZoneParams();
+    }
+
+    private void UpdateZoneParams()
+    { 
+        if (!targetManager || SegmentNumber + 1 == 0) return;
+        try
+        {
+            goalPosition = targetManager.zoneCentroids[SegmentNumber + 1];
+            zoneBounds = targetManager.zoneBounds[SegmentNumber + 1];
+            DrawZoneOutline();
+        }
+        catch (KeyNotFoundException ignored) {}
     }
     
     public void DrawCircleSegment(int steps, int stepsPerSegments, float radius, int segmentNumber) {
+        this.name = "Segment" + segmentNumber;
         this.SegmentNumber = segmentNumber;
         segmentRenderer.positionCount = stepsPerSegments + 1;
         Gradient originalColour= new Gradient();
@@ -56,6 +89,10 @@ public class Segment : MonoBehaviour
         int startStep = segmentNumber * stepsPerSegments;
         int endStep = (segmentNumber + 1) * stepsPerSegments;
         
+        var centrePosition = new Vector3();
+        var actualIndex = 0;
+        var interval = endStep - startStep;
+        var createdText = false;
         for (int currentstep = startStep; currentstep <= endStep; currentstep++)
         {
             // currentstep determines the progress of the circumference
@@ -70,9 +107,24 @@ public class Segment : MonoBehaviour
             float x = xScaled * radius;
             float y = yScaled * radius;
 
+            if (actualIndex >= interval/2 && !createdText)
+            {
+                createdText = true;
+                centrePosition = new Vector3(x, y, 0);
+                if (currentText)
+                {
+                    Destroy(currentText);
+                }
+                currentText = Instantiate(segmentText, centrePosition, Quaternion.identity);
+                currentText.GetComponent<TextMeshPro>().text = (SegmentNumber + 1).ToString();
+                currentText.transform.parent = transform;
+            }
+
             Vector3 currentPosition = new Vector3(x, y, 0);
             segmentRenderer.SetPosition(currentstep - startStep, currentPosition);
+            actualIndex++;
         }
+        
         
         // sort the points to form a polygon
         Mesh mesh = new Mesh();
@@ -84,6 +136,28 @@ public class Segment : MonoBehaviour
             colliderPoints2D[i] = new Vector2(colliderPoints[i].x, colliderPoints[i].y); // convert this to collider points
         }
         segmentCollider.points = colliderPoints2D; // set the collider points
+    }
+
+    private void DrawZoneOutline()
+    {
+        List<Vector3[]> boundingPairs = new List<Vector3[]>
+        {
+            new Vector3[] {new (zoneBounds.x, zoneBounds.z, 0), new (zoneBounds.y, zoneBounds.z, 0)},
+            new Vector3[] {new (zoneBounds.y, zoneBounds.z, 0), new (zoneBounds.y, zoneBounds.w, 0)},
+            new Vector3[] {new (zoneBounds.y, zoneBounds.w, 0), new (zoneBounds.x, zoneBounds.w, 0)},
+            new Vector3[] {new (zoneBounds.x, zoneBounds.w, 0), new (zoneBounds.x, zoneBounds.z, 0)}
+        };
+        for (int i = 0; i < 4; i++)
+        {
+            var lineRenderer = boundingBoxes[i].GetComponent<LineRenderer>();
+            if (!lineRenderer) break;
+            lineRenderer.positionCount = 2;
+            var pair = boundingPairs[i];
+            lineRenderer.startWidth = 0.05f;
+            lineRenderer.endWidth = 0.05f;
+            lineRenderer.SetPosition(0, pair[0]);
+            lineRenderer.SetPosition(1, pair[1]);
+        }
     }
 
     private void OnMouseExit()
@@ -105,11 +179,19 @@ public class Segment : MonoBehaviour
             );
         }
         segmentRenderer.colorGradient = originalColour;
+        foreach (var boundingBox in boundingBoxes)
+        {
+            boundingBox.SetActive(false);
+        }
     }
 
     private void OnMouseOver()
     {
         segmentRenderer.colorGradient = onHover;
+        foreach (var boundingBox in boundingBoxes)
+        {
+            boundingBox.SetActive(true);
+        }
     }
 
     public void setSegmentNumber(int segmentNumber)
